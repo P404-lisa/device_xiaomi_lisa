ac806c541f701f0e92138289a8106810f6a96c5a
bheatleyyy <sugakesshaa@gmail.com>
2022-07-06 20:14:23 +0800

    lisa: Switch to QTI common WiFi Display

--- device.mk
+++ device.mk
@@ -348,7 +348,8 @@ TARGET_COMMON_QTI_COMPONENTS := \
     nq-nfc \
     perf \
     telephony \
-    vibrator
+    vibrator \
+    wfd
 
 # Recovery
 PRODUCT_COPY_FILES += \
@@ -447,11 +448,3 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
     frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml
-
-# WiFi Display
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
-PRODUCT_PACKAGES += \
-    libnl \
-    libwfdaac_vendor
